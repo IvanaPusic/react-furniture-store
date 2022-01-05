235f22ae1afd313a7b3ec955b77f34d47280e234
Ivana Pusic <pusicka97@gmail.com>
2022-01-05 16:42:58 +0100

    initial commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="about img" />
+        <article>
+          <div className="title">
+            <h2>Our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+          non consectetur a erat nam at lectus urna duis convallis convallis tellus id interdum velit laoreet id donec ultrices tincidunt arcu non sodales neque sodales ut etiam sit amet nisl purus in mollis nunc sed id semper risus in hendrerit gravida rutrum quisque non tellus orci ac auctor augue mauris
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+export default AboutPage;
---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import AboutPage from "./AboutPage";
+import AuthWrapper from "./AuthWrapper";
+import CartPage from "./CartPage";
+import CheckOutPage from "./CheckoutPage";
+import ErrorPage from "./ErrorPage";
+import HomePage from "./HomePage";
+import PrivateRoute from "./PrivateRoute";
+import ProductsPage from "./ProductsPage";
+import SingleProductPage from "./SingleProductPage";
+
+export {
+  AboutPage,
+  AuthWrapper,
+  CartPage,
+  CheckOutPage,
+  ErrorPage,
+  HomePage,
+  PrivateRoute,
+  ProductsPage,
+  SingleProductPage,
+};
